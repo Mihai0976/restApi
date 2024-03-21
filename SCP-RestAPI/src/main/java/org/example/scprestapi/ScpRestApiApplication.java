b23e8d44141f23f840027366ffc220e76670e4cd
Josefine-Ha <josefine.hansson@ha.ax>
2024-03-21 14:47:53 +0200

    Skapat Spring projekt

---
+++ SCP-RestAPI/src/main/java/org/example/scprestapi/ScpRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.scprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScpRestApiApplication.class, args);
+	}
+
+}
